ff5156885234a4895cf6fd70cdbf89cc1f34178c
creepersaur <69436784+creepersaur@users.noreply.github.com>
2025-10-11 23:44:33 +0530

    last

--- obj/Release/net10.0/Cranberry.AssemblyInfo.cs
+++ obj/Release/net10.0/Cranberry.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cranberry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b437bda7b9a8f6569b15cf27b8a98d65623d6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e025ca086f209754dbfb00db476a7991d97f840c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cranberry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cranberry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
